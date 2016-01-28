bbfb9807add143c783dd9f20c92a0cc7ad9a3031
Anandh.Asokan <Anandh.Asokan@MPS-ASOKAN.perficient.com>
2016-01-28 14:34:29 -0600

    Removed Test Class and add Sample class with Junit class

---
+++ SampleCICDRepo/SampleCICD/src/com/company/testfiles/Sample.java
@@ -0,0 +1,8 @@
+package com.company.testfiles;
+
+public class Sample {
+
+	public void testFunction (int i){
+		System.out.println("this is a test " + i );
+	}
+}
---
+++ SampleCICDRepo/SampleCICD/src/com/company/testfiles/TestTest.java
@@ -0,0 +1,19 @@
+package com.company.testfiles;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.sun.net.httpserver.Authenticator.Success;
+
+import junit.framework.TestCase;
+
+public class TestTest  extends TestCase{
+
+	@Test
+	public void testMain() {
+		 Sample t = new Sample();
+		 t.testFunction(1);
+	}
+
+}
